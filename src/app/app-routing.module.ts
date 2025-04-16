e0423c49c0df14dc37c8d2d5f9ab2feeeb4563a2
PanthGadani <panth@coronation.in>
2025-04-16 12:02:35 +0530

    3 sections completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { title } from 'process';
+import { SofitelDetailPageComponent } from './sofitel-detail-page/sofitel-detail-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+    // path: '',
+    // component: AppComponent,
+    // data: {title: 'R&R Group'},
+    // children: [
+    //   {
+    //     path:'/a',
+    //   },
+    //   {
+    //     path:'/b',
+    //   }
+    // ]
+  // }
+  {
+    path: 'sofitel',
+    component: SofitelDetailPageComponent,
+    data: {title: 'Rehvassa Sofitel'}
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
