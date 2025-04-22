3198ab1eccdda651837087db657daec2a7945979
PanthGadani <panth@coronation.in>
2025-04-22 17:49:27 +0530

    completed Form Validations in all components

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
@@ -11,5 +12,29 @@ export class ContactPageComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  formSubmitted = false;
+
+  inquireForm = new FormGroup({
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', Validators.required),
+    message: new FormControl(''),
+  });
+
+  restrictNonNumeric(event: KeyboardEvent) {
+    const regex = /[0-9]/;
+    if (!regex.test(event.key)) {
+      event.preventDefault();
+    }
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+    if (this.inquireForm.invalid) {
+      return;
+    }
+  }
+
 
 }
--- src/app/inquire-page/inquire-page.component.ts
+++ src/app/inquire-page/inquire-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inquire-page',
@@ -10,6 +11,33 @@ export class InquirePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  formSubmitted = false;
+
+  inquireForm = new FormGroup({
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+    budget: new FormControl('', Validators.required),
+    type: new FormControl('', Validators.required)
+  });
+
+  restrictNonNumeric(event: KeyboardEvent) {
+    const regex = /[0-9]/;
+    if (!regex.test(event.key)) {
+      event.preventDefault();
+    }
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+
+    if (this.inquireForm.invalid) {
+      return;
+    }
+
   }
 
 }
