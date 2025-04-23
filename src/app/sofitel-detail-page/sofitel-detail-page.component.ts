5b5aed694911c82c0dabc7bc1423de475a264b49
Parmaryash-7 <parmars4679@gmail.com>
2025-04-23 16:02:27 +0530

    some changes

--- src/app/sofitel-detail-page/sofitel-detail-page.component.ts
+++ src/app/sofitel-detail-page/sofitel-detail-page.component.ts
@@ -90,7 +90,7 @@ export class SofitelDetailPageComponent implements AfterViewInit {
 
     Swiper.use([Pagination]);
     new Swiper(this.swiperContainerCounter.nativeElement, {
-      slidesPerView: "auto",
+      slidesPerView: 2,
       spaceBetween: 0,
       pagination: {
         el: ".swiper-pagination",
